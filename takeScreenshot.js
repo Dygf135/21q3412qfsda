e287ab8021ea4beaa9fed853b6d0ee6f1d28feac
hmltmadu <hmltmadu@gmail.com>
2024-07-06 17:48:47 +0000

    Add screenshot script and update workflow

---
+++ takeScreenshot.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: true,
+    args: [
+      '--no-sandbox'
+    ],
+    executablePath: process.env.PUPPETEER_EXEC_PATH || puppeteer.executablePath(),
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://example.com", {
+    waitUntil: "networkidle0",
+  });
+
+  // Take a screenshot
+  await page.screenshot({ path: "screenshot.png" });
+  console.log("Screenshot taken.");
+
+  await browser.close();
+}
+
+run();
